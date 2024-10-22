d60017d7fd2d9705af8a6fc6691cf3791f337940
21120036 <thienan2003bt@gmail.com>
2024-10-22 23:26:41 +0700

    Add assets and set up for tailwindcss in react native using nativewind.

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,11 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world from Aora!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-black">Hello world from Aora!</Text>
             <StatusBar style="auto" />
             <Link href={"/profile"} style={{ color: 'blue' }}>
                 Go to profile
@@ -13,13 +13,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
